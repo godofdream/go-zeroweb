958a3653734095214d183f4df46e514aba7f2954
Kosta <theoneandonlykosta@gmail.com>
2018-07-03 17:10:43 +0200

    Allow configuration of http.network, e.g. to tcp6 (#8)

--- serve.go
+++ serve.go
@@ -13,13 +13,15 @@ func (a *Zeroweb) Serve() error {
 		FastOpen:    true,
 		Backlog:     a.Config.GetInt("http.max_pending_connections"),
 	}
-	ln, err := tcpconf.NewListener("tcp", a.Config.GetString("http.addr"))
+	network := a.Config.GetString("http.network")
+	addr := a.Config.GetString("http.addr")
+	ln, err := tcpconf.NewListener(network, addr)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed starting Listener")
 		return err
 	}
 
-	log.Info().Msg("Starting Webserver")
+	log.Info().Str("network", network).Str("addr", addr).Msg("Starting Webserver")
 	if err = a.Server.Serve(ln); err != nil {
 		log.Fatal().Err(err).Msg("Failed serving incoming connections")
 		return err
